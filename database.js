f681e7fbcd94634c5a79db48de2ac5f77b58c61a
https://github.com/MD-Hussain-Akhtar <mhussainakhter@gmail.com>
2026-05-14 13:09:15 -0700

    final mongodb fix

--- database.js
+++ database.js
@@ -1,24 +1,30 @@
 const { MongoClient } = require("mongodb");
 
-const uri =
-"mongodb://mhussainakhter:Shaddam%40123@ac-nmsw8zk-shard-00-00.78rwg7r.mongodb.net:27017,ac-nmsw8zk-shard-00-01.78rwg7r.mongodb.net:27017,ac-nmsw8zk-shard-00-02.78rwg7r.mongodb.net:27017/studentDB?ssl=true&replicaSet=atlas-4ljvmr-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Hussain";
-
+const uri ="mongodb+srv://mhussainakhter:Hussain123@hussain.78rwg7r.mongodb.net/studentDB?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
 let db;
 
 async function connectDB() {
+
+  if (db) return db;
+
   try {
+
     await client.connect();
 
     console.log("✅ MongoDB Connected");
 
     db = client.db("studentDB");
 
+    return db;
+
   } catch (error) {
-    console.log("❌ Database Connection Error");
+
     console.log(error);
+
   }
+
 }
 
 function getDB() {
